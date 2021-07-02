335b962ba5a53bda00f7948b7198e8d125d33cf6
Thierry <thierrybarbosasantos@gmail.com>
2021-07-02 04:28:21 -0300

    hope it works

--- src/script/coclima2.js
+++ src/script/coclima2.js
@@ -1,32 +1,8 @@
-/*(function (w, d, s, o, f, js, fjs) {
-  w.PopConvert = o;
-  w[o] =
-  w[o] ||
-     function () {
-      (w[o].q = w[o].q || []).push(arguments);
-    };
-  (js = d.createElement(s)), (fjs = d.getElementsByTagName(s)[0]);
-  js.id = o;
-  js.src = f;
-  js.async = 1;
-  fjs.parentNode.insertBefore(js, fjs);
-})(
-  window,
-  document,
-  "script",
-  "pcw",
-  "https://cdn.popconvert.com.br/widget/popconvert.js"
-);*/
-
-(function (w, d, s, h, g, a, m) {
-  (w[g] =
-    w[g] ||
-    function () {
-      (w[g].q = w[g].q || []).push(arguments);
-    }),
-    (w[g].l = 1 * new Date());
-  (a = d.createElement(s)), (m = d.getElementsByTagName(s)[0]);
-  a.async = 1;
-  a.src = h;
-  m.parentNode.insertBefore(a, m);
-})(window, document, "script", "//api.coclima.com/script");
+(function () {
+  const script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.async = true;
+  script.src = 'https://api.coclima.com/script';
+  const s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(script, s);
+}());
